f98dd84365f01192936feb0ba54219aa99fd4f9d
nuhkoca <nuhkocaa@gmail.com>
2018-06-26 14:48:40 +0300

    switch to DataBinding v2

--- app/src/main/java/com/nuhkoca/mvvmrxjavaretrofitdatabindingdemo/data/dao/EverythingDao.java
+++ app/src/main/java/com/nuhkoca/mvvmrxjavaretrofitdatabindingdemo/data/dao/EverythingDao.java
@@ -3,6 +3,7 @@ package com.nuhkoca.mvvmrxjavaretrofitdatabindingdemo.data.dao;
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.nuhkoca.mvvmrxjavaretrofitdatabindingdemo.data.entity.DbEverything;
